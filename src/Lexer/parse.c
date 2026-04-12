36b1b029d0acf36ff799a60b28d8a59ea33d4115
Miloud-UX <basicalllymiloud@gmail.com>
2026-04-12 21:59:35 +0100

    Fix error propagation throughout parse_create functions  Create parse relationship  add entity info struct  add relationship info struct  CreateCommand info struct  Change return type of create functions Added execute_command function to seperate parsing from executing  Atomic relationship creation is done

--- src/Lexer/parse.c
+++ src/Lexer/parse.c
@@ -3,6 +3,7 @@
 #include "../command_processor.h"
 #include "ncurses.h"
 #include <stdio.h>
+#include <string.h>
 
 void init_parser(Parser *p) {
     if (!p) {
@@ -12,9 +13,7 @@ void init_parser(Parser *p) {
     p->count = 0;
 }
 
-void error_msg(WINDOW *console_win, int pos, const char *error) {
-    show_message(console_win, error);
-}
+void error_msg(WINDOW *console_win, int pos, const char *error) { show_message(console_win, error); }
 
 void parse_command(Parser *p, const char *content, WINDOW *console_win) {
     // Root function that calls other child functions
@@ -24,14 +23,15 @@ void parse_command(Parser *p, const char *content, WINDOW *console_win) {
     init_parser(p);
     tokenize_content(content, p->tokens, &p->count);
 
-    // create entity "John"
-
     while (p->current < p->count) {
         Token t = peek_token(p->tokens, p->current);
         if (t.type == TOKEN_CREATE) {
             // consume token
+            CreateCommand c = {0};
             advance_token(&p->current);
-            parse_create(p, console_win);
+            if (parse_create(p, console_win, &c)) {
+                execute_create(c);
+            }
         } else if (t.type == TOKEN_HELP) {
             // parse_help(p);
         } else if (t.type == TOKEN_CLEAR) {
@@ -48,40 +48,108 @@ void parse_command(Parser *p, const char *content, WINDOW *console_win) {
             //          "Unknown token type = %d, value=%s", t.type, t.value);
             // show_message(console_win, debug_msg);
 
-            // const char *error = "Unknown command try : {create, help,clear}";
-            // error_msg(console_win, p->tokens[p->current].pos, error);
+            const char *error = "Unknown command try : {create, help,clear}";
+            error_msg(console_win, p->tokens[p->current].pos, error);
             return;
         }
     }
 }
 
-void parse_create(Parser *p, WINDOW *console) {
+bool parse_create(Parser *p, WINDOW *console, CreateCommand *c) {
     Token t = peek_token(p->tokens, p->current);
     if (t.type == TOKEN_ENTITY) {
         advance_token(&p->current);
-        parse_create_entity(console, p);
+        if (parse_create_entity(console, p, false, c))
+            return true;
     } else if (t.type == TOKEN_RELATIONSHIP) {
-        // advance_token(&p->current);
-        // parse_create_relationship(Parser * p);
+        advance_token(&p->current);
+        if (parse_create_relationship(console, p, c))
+            return true;
     } else {
         // Error expected : entity or relationship
-        const char *error =
-            "Syntax Error : expected either Entity or relationship";
+        const char *error = "Syntax Error : expected either Entity or relationship";
         error_msg(console, p->current, error);
-        return;
     }
+    return false;
 }
 
-void parse_create_entity(WINDOW *console, Parser *p) {
+// bool is_called is used to ignore "advance_token()" since we are already
+// advancing from the caller.
+
+bool parse_create_entity(WINDOW *console, Parser *p, bool is_called, CreateCommand *c) {
     Token t = peek_token(p->tokens, p->current);
     if (t.type == TOKEN_STRING) {
-        advance_token(&p->current);
-        createEntity(t.value, 10, 20);
+        if (!is_called) {
+            advance_token(&p->current);
+        }
+        c->type = TYPE_ENTITY;
+        strncpy(c->Data.e.name, t.value, MAX_NAME_LEN);
+        c->Data.e.name[MAX_NAME_LEN - 1] = '\0';
+        return true;
     } else {
         const char *error = "Syntax Error : expected entity name";
-        // TODO : change return type to bool to propagate and
-        // remove the ghost effect of overwriting the errors
         error_msg(console, p->current, error);
     }
-    return;
+    return false;
+}
+// TODO : Add cardinality tokenization
+// create relationship "job" "employee" "company"
+
+bool parse_create_relationship(WINDOW *console, Parser *p, CreateCommand *c) {
+    Token t = peek_token(p->tokens, p->current);
+    if (t.type == TOKEN_STRING) {
+        // get entities and then add relationship
+        advance_token(&p->current);
+        bool Create_e1 = parse_create_entity(console, p, true, c);
+        char e1_name[MAX_NAME_LEN];
+
+        if (!Create_e1) {
+            return false;
+        }
+        strncpy(e1_name, c->Data.e.name, MAX_NAME_LEN);
+        e1_name[MAX_NAME_LEN - 1] = '\0';
+
+        advance_token(&p->current);
+
+        bool Create_e2 = parse_create_entity(console, p, true, c);
+        char e2_name[MAX_NAME_LEN];
+
+        if (!Create_e2) {
+            return false;
+        }
+
+        // Both are succesful so we create the relationship
+
+        strncpy(e2_name, c->Data.e.name, MAX_NAME_LEN);
+        e2_name[MAX_NAME_LEN - 1] = '\0';
+        advance_token(&p->current);
+
+        c->type = TYPE_RELATIONSHIP;
+        strncpy(c->Data.r.name, t.value, MAX_NAME_LEN);
+        c->Data.e.name[MAX_NAME_LEN - 1] = '\0';
+
+        strncpy(c->Data.r.e1_name, e1_name, MAX_NAME_LEN);
+        c->Data.r.e1_name[MAX_NAME_LEN - 1] = '\0';
+
+        strncpy(c->Data.r.e2_name, e2_name, MAX_NAME_LEN);
+        c->Data.r.e2_name[MAX_NAME_LEN - 1] = '\0';
+
+        return true;
+    } else {
+        const char *error = "Syntax Error : expected Relationhip name";
+        error_msg(console, p->current, error);
+    }
+    return false;
+}
+
+void execute_create(CreateCommand c) {
+    if (c.type == TYPE_ENTITY) {
+        createEntity(c.Data.e.name, 10, 10);
+    } else {
+        Entity *e1 = createEntity(c.Data.r.e1_name, 10, 10);
+        Entity *e2 = createEntity(c.Data.r.e2_name, 10, 10);
+        if (!e1 || !e2)
+            return;
+        addRelationship(10, 10, e1, e2, c.Data.r.name);
+    }
 }
--- src/Lexer/parse.h
+++ src/Lexer/parse.h
@@ -1,5 +1,6 @@
 #ifndef SRC_PARSE_H
 #define SRC_PARSE_H
+#include "../MCD_elements.h"
 #include "tokenize.h"
 #include <ncurses.h>
 
@@ -9,12 +10,36 @@ typedef struct {
         int current;
 } Parser;
 
+typedef struct {
+        char name[MAX_NAME_LEN];
+} EntityInfo;
+
+typedef struct {
+        char name[MAX_NAME_LEN];
+        char e1_name[MAX_NAME_LEN];
+        char e2_name[MAX_NAME_LEN];
+} RelationshipInfo;
+
+typedef enum { TYPE_ENTITY, TYPE_RELATIONSHIP } CreateType;
+
+typedef struct {
+        CreateType type;
+        union {
+                EntityInfo e;
+                RelationshipInfo r;
+        } Data;
+} CreateCommand;
+
 void init_parser(Parser *p);
 void parse_command(Parser *p, const char *content, WINDOW *console_win);
 // maybe in future implementation of error msg function we can add a simple
 // guess 'did you mean this "TOKEN"' then we have to pass the current token
 void error_msg(WINDOW *console_win, int pos, const char *error);
-void parse_create(Parser *p, WINDOW *console);
-void parse_create_entity(WINDOW *console, Parser *p);
+bool parse_create(Parser *p, WINDOW *console, CreateCommand *c);
+bool parse_create_entity(WINDOW *console, Parser *p, bool is_called, CreateCommand *c);
+bool parse_create_relationship(WINDOW *console, Parser *p, CreateCommand *c);
+
+// to be moved to a seperate file for execution
+void execute_create(CreateCommand c);
 
 #endif // !SRC_PARSE_H
